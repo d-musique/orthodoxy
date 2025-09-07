1d524897a6c6af9d4b8fc0ed290b1b9c51ea76c6
D.musique <d-musique@users.noreply.github.com>
2025-09-07 17:30:51 +0200

    Cleanup

--- src/diag.cpp
+++ src/diag.cpp
@@ -1,11 +1,11 @@
 // Copyright: D.M., 2025-present
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include "diag.h"
+
 #define DEFINE_DIAGNOSTIC(id, alt, msg) \
-    namespace Orthodoxy { namespace diag { \
-        extern const OrthodoxyDiagDesc id; } } \
-    \
     const OrthodoxyDiagDesc Orthodoxy::diag::id \
         { #id, #alt, msg " [orthodoxy::" #alt "]" }
 
-#include "diag.h"
+#include "diag.inc"
+#undef DEFINE_DIAGNOSTIC
